9b8e125a47c01019a1d7d2499fba0f7266ec6546
jess-miz <JessicaRmisra@gmail.com>
2023-08-18 10:00:24 -0500

    Create sumOfTwoLowestPositiveNums.js

---
+++ 7th Kyu/sumOfTwoLowestPositiveNums.js
@@ -0,0 +1,17 @@
+/*Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+*/
+
+
+// Answer //
+
+ function getSumOfTwoLowestNums(arr){
+
+    arr = arr.sort((a,b) => a - b)
+    return arr[0] + arr[1]
+ }
+
+ getSumOfTwoLowestNums([1, 4, 12, 20]) // 5
+ getSumOfTwoLowestNums([]) // 0
