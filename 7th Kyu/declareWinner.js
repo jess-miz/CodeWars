5ccda048a3ebda0cd8213fe72cdc9935fd47a6ab
jess-miz <JessicaRmisra@gmail.com>
2023-09-21 13:35:24 -0500

    Create declareWinner.js

---
+++ 7th Kyu/declareWinner.js
@@ -0,0 +1,32 @@
+/* 
+Create a function that returns the name of the winner in a fight between two fighters.
+
+Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+Your function also receives a third argument, a string, with the name of the fighter that attacks first.
+
+*/
+
+function Fighter (name, health, damagePerAttack){
+    this.name = name
+    this.health = health
+    this.damagePerAttack = damagePerAttack
+    this.toString = function() { return this.name }
+}
+
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+  let fac1 = Math.ceil( fighter1.health / fighter2.damagePerAttack );
+  let fac2 = Math.ceil( fighter2.health / fighter1.damagePerAttack );
+  if(fac1 < fac2) {
+    return fighter2.name;
+  } else if(fac2 < fac1) {
+    return fighter1.name;
+  } else {
+    return firstAttacker;
+  }
+}
