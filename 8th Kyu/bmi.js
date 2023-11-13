bd14310409fcd600ea7a5c3cc69865fba35c0727
jess-miz <JessicaRmisra@gmail.com>
2023-11-13 14:39:52 -0600

    Update bmi.js

--- 8th Kyu/bmi.js
+++ 8th Kyu/bmi.js
@@ -13,6 +13,6 @@ if bmi > 30 return "Obese"
 
 // Answer // 
 function bmi(weight, height) {
-    let bmi =weight /  Math.pow((height),2);
-    return bmi<=18.5?"Underweight":bmi<=25?"Normal":bmi<=30?"Overweight":"Obese";
+    let bmi = weight /  Math.pow((height),2);
+    return bmi <= 18.5 ? "Underweight" : bmi<=25 ? "Normal" : bmi<=30 ? "Overweight" : "Obese";
 }
