b32c655138bebaaf63b0f9c7ff3126948a5b3ae7
jess-miz <JessicaRmisra@gmail.com>
2023-12-06 14:29:00 -0600

    Create arrayFilter.js

---
+++ 7th Kyu/arrayFilter.js
@@ -0,0 +1,6 @@
+/*
+JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+*/
+
+// Answer // 
+const getEvenNumbers = numbersArray => numbersArray.filter(num => num % 2 == 0);
