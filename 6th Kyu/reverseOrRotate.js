81db631a93da060fd1e16a94c9d8c04d775cefb1
jess-miz <JessicaRmisra@gmail.com>
2023-12-19 12:32:31 -0600

    Update reverseOrRotate.js

--- 6th Kyu/reverseOrRotate.js
+++ 6th Kyu/reverseOrRotate.js
@@ -13,7 +13,7 @@ If
 // Answer // 
 function revrot(str, sz) {
     if(!sz||sz<0||sz>str.length)return '';
-    var arr=str.match(new RegExp('\\d{'+sz+'}','g'));
+    let arr=str.match(new RegExp('\\d{'+sz+'}','g'));
     return arr.map(function(v){
       if(v.toString().split('').reduce((s,v)=>s+v*1,0)%2===0){
         return v.split('').reverse().join('');
