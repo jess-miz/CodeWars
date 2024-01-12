d2155b44ca1d6d004aa2566ff88124b238a6cd7f
jess-miz <JessicaRmisra@gmail.com>
2024-01-12 17:28:55 -0600

    created diagonal_differences.py

--- Python/Hackerrank/diagonal_differences.py
+++ Python/Hackerrank/diagonal_differences.py
@@ -1,10 +1,16 @@
-""""
-
 def diagonal_difference(arr):
     l_sum = 0 
     r_sum = 0
     for i in range(len(arr)):
         l_sum = l_sum + arr[i][i]
         r_sum = r_sum + arr[(len(arr) - 1) - i]
-    return abs(l_sum - r_sum) """
+    return abs(l_sum - r_sum)
 
+
+
+
+
+
+
+        
+        
