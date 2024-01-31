66dd6af53fef476a77894eedacb5f704c526b40f
jess-miz <JessicaRmisra@gmail.com>
2024-01-31 11:00:04 -0600

    Create bumpsInRoad.js

---
+++ JavaScript/7th Kyu/bumpsInRoad.js
@@ -0,0 +1,11 @@
+/*
+Your car is old, it breaks easily. The shock absorbers are gone and you think it can handle about 15 more bumps before it dies totally.
+
+Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+*/
+
+// Answer // 
+function bump(x){
+    let bumps = x.split('').filter(x => x === 'n').length
+    return bumps <= 15 ? 'Woohoo!' : 'Car Dead'
+}
